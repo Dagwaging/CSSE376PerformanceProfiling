660396ad4a1c4d9530566663092d0bee533bd6fc
Dagwaging <dagwaging@gmail.com>
2015-05-05 14:00:32 -0400

    Add memoization to MusicPlayer

--- src/toolObjects/MusicPlayer.java
+++ src/toolObjects/MusicPlayer.java
@@ -16,13 +16,20 @@ public class MusicPlayer {
 	
 	private Clip clip;
 	
+	static HashMap<String, Clip> clips = new HashMap<String ,Clip>();
+	
 	/**
 	 * Constructs a Music Player.
 	 *
 	 * @param fileName the destination of the media file.
 	 */
 	public MusicPlayer(String fileName) {
-		// FIXME: reduce the number of calls to the code below
+		// try to obtain existing clip first.
+		if(clips.containsKey(fileName)) {
+			this.clip = clips.get(fileName);
+			return;
+		}
+
 		// Obtain a clip.
 		try {
 			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getResource(fileName));
@@ -39,6 +46,8 @@ public class MusicPlayer {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		clips.put(fileName, clip);
 	}
 	
 	/**
